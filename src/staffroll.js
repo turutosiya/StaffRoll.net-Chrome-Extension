923e811092a606eaf565e20b89b589271ac0a1c0
Toshiya TSURU <turutosiya@gmail.com>
2010-12-04 15:51:18 +0900

    aa

---
+++ src/staffroll.js
@@ -0,0 +1,7 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+  alert('tabs.onCreated --'
+              + ' window: ' + tab.windowId
+              + ' tab: '    + tab.id
+              + ' index: '  + tab.index
+              + ' url: '    + tab.url);
+});
